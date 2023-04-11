c85db28b2caef406b510c292169dedec9e2ed512
daryabaldina1702 <71131519+daryabaldina1702@users.noreply.github.com>
2023-04-11 23:42:20 +0300

    Second solution of task3

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ from scipy.stats import ttest_ind
 chat_id = 834411281 # Ваш chat ID, не меняйте название переменной
 
 def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    statistic, pvalue = ttest_ind(x, y)
+    statistic, pvalue = ttest_ind(x, y, equal_var=False, alternative="less")
     alpha = 0.09
     return pvalue < alpha
