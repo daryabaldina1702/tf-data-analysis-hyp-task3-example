bb7f4f089d7c1084a7e0dda1e1a696ce4e101cd5
daryabaldina1702 <71131519+daryabaldina1702@users.noreply.github.com>
2023-04-12 19:30:08 +0300

    Forth solution of task3

--- solution.py
+++ solution.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
-from scipy.stats import t
+import math
+import scipy.stats
 
 chat_id = 834411281 # Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x_success,
+             y_success) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha=0.09
-    n_control = len(x)
-    n_test = len(y)
-    mean_control = sum(x) / n_control
-    mean_test = sum(y) / n_test
-    var_control = sum((i - mean_control)**2 for i in x) / (n_control - 1)
-    var_test = sum((x - mean_test)**2 for x in y) / (n_test - 1)
-    s = (var_control/n_control + var_test/n_test)**0.5
-    t_crit = t.ppf(1 - alpha/2, n_control + n_test - 2)
-    t = (mean_test - mean_control) / s*(1/n_control + 1/n_test)**0.5
-    return True if abs(t) > t_crit else False
+    n_control = len(x_success)
+    n_test = len(y_success)
+    mean_control = sum(x_success) / n_control
+    mean_test = sum(y_success) / n_test
+    var_control = sum((x - mean_control)**2 for x in x_success) / (n_control - 1)
+    var_test = sum((x - mean_test)**2 for x in y_success) / (n_test - 1)
+    s = math.sqrt(var_control/n_control + var_test/n_test)
+    t_crit = scipy.stats.t.ppf(1 - alpha/2, n_control + n_test - 2)
+    t = (mean_test - mean_control) / s*math.sqrt(1/n_control + 1/n_test)
+    return True if abs(t) > t_crit else False 
